5aeca0792629b404d31be63ff8d331cfcae56567
jeremy <kepung@gmail.com>
2016-12-23 22:40:30 +1300

    updated project with typings. added login components and bootstrapping

--- AngularV2/src/WebApplication1/wwwroot/app/app.module.ts
+++ AngularV2/src/WebApplication1/wwwroot/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, LoginComponent ],
+  bootstrap: [AppComponent, LoginComponent ]
 })
 export class AppModule { }
---
+++ AngularV2/src/WebApplication1/wwwroot/app/login/login.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-login',
+    templateUrl: 'app/login/login.component.html'
+})
+
+export class LoginComponent { name = 'Jeremy'; }
