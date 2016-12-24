40680826b1d289660d033fb7cb1f4231bed5b6db
jeremy <kepung@gmail.com>
2016-12-25 07:46:26 +1300

    form login enhanced with effects.

--- AngularV2/src/WebApplication1/wwwroot/_references.js
+++ AngularV2/src/WebApplication1/wwwroot/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="app/app.component.js" />
 /// <reference path="app/app.module.js" />
+/// <reference path="app/login/login.component.js" />
 /// <reference path="app/main.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
